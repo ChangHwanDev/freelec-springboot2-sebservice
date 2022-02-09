4c700d22a6c061fde5157b1d5d7587119334336d
[denniemate] <[wjdckdcjd@gmail.com]>
2022-02-09 18:50:38 +0900

    간단한 test 확인

---
+++ src/main/java/pw/jch/www/springboot/Application.java
@@ -0,0 +1,11 @@
+package pw.jch.www.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
